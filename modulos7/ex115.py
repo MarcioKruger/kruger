ea9d010e40d182054d659c8119ca2058bb4e1450
MarcioKruger <ms.kruger@hotmail.com>
2023-07-28 23:42:49 -0300

    exercicios

--- modulos7/ex115.py
+++ modulos7/ex115.py
@@ -1,11 +1,21 @@
 import time
-from utilidades import moeda, menu
+from utilidades import arquivo, menu, moeda
+
+arq = 'documento.txt'
+
+if not arquivo.arquivoExiste(arq):
+    arquivo.criarArquivo(arq)
+
 while True:
     resposta = menu.menu(['Lista pessoas', 'Cadastrar pessoa', 'SAIR'])
     if resposta == 1:
         menu.opcao1(1)
+        arquivo.lerArquivo(arq)
     elif resposta == 2:
         menu.opcao2(2)
+        nome = str(input('Nome: '))
+        idade = moeda.leiaInt('Idade: ')
+        arquivo.cadastrar(arq, nome, idade)
     elif resposta == 3:
         break
     time.sleep(2)
